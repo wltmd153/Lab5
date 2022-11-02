d981b15ca4dc34368035bd631df9011d29b70271
wltmd153 <103233907+wltmd153@users.noreply.github.com>
2022-11-01 19:21:27 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,48 @@
 // explore.js
+// was not able to implement smiling
+// Got reference from https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+const synth = window.speechSynthesis;
+const inputTxt = document.querySelector('.txt');
+const voiceSelect = document.querySelector('select');
 
+
+let voices = [];
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
 window.addEventListener('DOMContentLoaded', init);
 
+//document.getElementById("text-to-speak").value = "Jiseung Yoo"
 function init() {
-  // TODO
+   var button = document.querySelector("button");
+   button.addEventListener('click', clickOnButton);
+}
+
+function clickOnButton(){
+  const utterThis = new SpeechSynthesisUtterance(document.getElementById("text-to-speak").value);
+  const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for (let i = 0; i < voices.length ; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  synth.speak(utterThis);
+}
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length ; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
 }
